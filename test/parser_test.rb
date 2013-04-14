961b1a684e6921bd57f1cc98d345b6fdce21cdb1
Willem van Bergen <willem@vanbergen.org>
2013-04-14 06:34:38 -0400

    Fixed some literal issues.

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -2,19 +2,36 @@ require 'test_helper'
 
 class ParserTest < Minitest::Unit::TestCase
 
-  include SQLToolkit
+  def assert_parses(sql)
+    ast = SQLToolkit.parse(sql) rescue nil
+    assert_instance_of Treetop::Runtime::SyntaxNode, ast, "Expected the provided string to parse as valid SQL"
+  end
+
+  def test_basic_syntax_flexibility
+    assert_parses('select c1')
+    assert_parses('SELECT c1')
+    assert_parses('SeLeCT c1')
+    assert_parses('  select    c1 , c2,    c3,c4   ')
+  end
+
+  def test_literals
+    assert_parses 'select true, false, null'
+    assert_parses "select 'test', 'test''with''quotes'"
+    assert_parses 'select 1, -1, 1.2, .001, -1.2, -.001'
+    assert_parses 'select c1 AS """Cool"" column", "c2"'
+  end
 
   def test_projections
-    assert parse(%q[    select 1, 'test', id, "id"  ])  
-    assert parse('select *')
-    assert parse('select table.*, other')
-    assert parse('select schema.table.*')
-    assert parse('select distinct schema.table.*')
-    assert parse('select min(table)')
-    assert parse('select count(*)')
-    assert parse('select count(distinct *)')
-    assert parse('select count(distinct id)')
-    assert parse('select count(1) AS number_of_records')
+    assert_parses('select 1, \'test\', id, "id"')
+    assert_parses('select *')
+    assert_parses('select table.*, other, "fields"')
+    assert_parses('select schema.table.*')
+    assert_parses('select distinct schema.table.*')
+    assert_parses('select min(table)')
+    assert_parses('select count(*)')
+    assert_parses('select count(distinct *)')
+    assert_parses('select count(distinct id)')
+    assert_parses('select count(1) AS number_of_records')
   end
 
   def test_set_operations
@@ -23,16 +40,16 @@ class ParserTest < Minitest::Unit::TestCase
   end
 
   def test_sources
-    assert parse('select * from t1')
-    assert parse('select * from table1 "t1"')
-    assert parse('select * from schema.table1 as t1')
-    assert parse('select * from table_1 as "first table", table_2 as "second table"')
+    assert_parses('select * from t1')
+    assert_parses('select * from table1 "t1"')
+    assert_parses('select * from schema.table1 as t1')
+    assert_parses('select * from table_1 as "first table", table_2 as "second table"')
   end
 
   def test_joins
-    assert parse('select * from table t1 join table t2 on t1.a = t2.a')
-    assert parse('select * from t1 full outer join t2 using (country, state)')
-    assert parse(<<-SQL)
+    assert_parses('select * from table t1 join table t2 on t1.a = t2.a')
+    assert_parses('select * from t1 full outer join t2 using (country, state)')
+    assert_parses(<<-SQL)
       SELECT *
         FROM table1 AS t1
         JOIN table2 AS t2 on t1.id = t2.id
@@ -44,65 +61,65 @@ class ParserTest < Minitest::Unit::TestCase
   end
 
   def test_subquery
-    assert parse('select a from (select b) as b_alias')
-    assert parse('select a from ( select b from (select c) as c_alias ) as b_alias')
+    assert_parses('select a from (select b) as b_alias')
+    assert_parses('select a from ( select b from (select c) as c_alias ) as b_alias')
   end
 
   def test_arithmetic_operators
-    assert parse("select 'a' + 'b'")
-    assert parse("select 'a' || ('b' || 'c') || 'd'")
-    assert parse('select 1 + 2 - (3 * 4)::float / 5 % 6')
+    assert_parses("select 'a' + 'b'")
+    assert_parses("select 'a' || ('b' || 'c') || 'd'")
+    assert_parses('select 1 + 2 - (3 * 4)::float / 5 % 6')
   end
 
   def test_comparison_operators
-    assert parse('select 1 > 2')
-    assert parse('select 1 + 2 > 2')
-    assert parse('select a > b')
-    assert_raises(RuntimeError) { parse('select 1 > 2 > 3') }
+    assert_parses('select 1 > 2')
+    assert_parses('select 1 + 2 > 2')
+    assert_parses('select a > b')
+    assert_raises(SQLToolkit::ParseError) { SQLToolkit.parse('select 1 > 2 > 3') }
   end
 
   def test_boolean_tests
-    assert parse('select column IS NOT TRUE')
-    assert parse('select column IS NULL')
+    assert_parses('select column IS NOT TRUE')
+    assert_parses('select column IS NULL')
   end
 
   def test_function_calls
-    assert parse('select MIN(column), now(), complicated_stuff(1, 4 + 2)')
+    assert_parses('select MIN(column), now(), complicated_stuff(1, 4 + 2)')
   end
 
   def test_in_construct
-    assert parse('select 1 IN (1,2,3)')
+    assert_parses('select 1 IN (1,2,3)')
   end
 
   def test_exist_construct
-    assert parse('select exist(select 1)')
-    assert parse('select not exist (select 1)')
+    assert_parses('select exist(select 1)')
+    assert_parses('select not exist (select 1)')
   end
 
   def test_boolean_operators
-    assert parse('select (a > b AND b > c) OR a IS NULL OR c IS NULL')
-    assert parse('select a >= 10 and b <= 0')
+    assert_parses('select (a > b AND b > c) OR a IS NULL OR c IS NULL')
+    assert_parses('select a >= 10 and b <= 0')
   end
 
   def test_where
-    assert parse("select * from t1 where a = 'test' and b >= 10")
-    assert parse('select a where (false)')
+    assert_parses("select * from t1 where a = 'test' and b >= 10")
+    assert_parses('select a where (false)')
   end  
 
   def test_group_by_and_having
-    assert parse('select a, b, min(c) min_c group by a, b')
-    assert parse('select a, b, min(c) min_c group by a, b having a >= 10 and min_c')
+    assert_parses('select a, b, min(c) min_c group by a, b')
+    assert_parses('select a, b, min(c) min_c group by a, b having a >= 10 and min_c')
   end
 
   def test_limit_offset
-    assert parse('select * from table limit 10')
-    assert parse('select * from table limit 10 offset 50')
+    assert_parses('select * from table limit 10')
+    assert_parses('select * from table limit 10 offset 50')
   end
 
   def test_comments
-    assert parse("select 1 -- comment\n")
-    assert parse("select -- comment\n-- more comments \n 1")
-    assert parse(<<-SQL)
+    assert_parses("select 1 -- comment\n")
+    assert_parses("select -- comment\n-- more comments \n 1")
+    assert_parses(<<-SQL)
       select 1,2,3,4 -- ... and so on
         from my_first_table,
              my_second_table
@@ -111,7 +128,7 @@ class ParserTest < Minitest::Unit::TestCase
   end
 
   def test_complicated_query
-    assert parse(<<-SQL)
+    assert_parses(<<-SQL)
 
       SELECT o.total_price_in_usd AS gmv,
              o.created_at AS gmv_at
